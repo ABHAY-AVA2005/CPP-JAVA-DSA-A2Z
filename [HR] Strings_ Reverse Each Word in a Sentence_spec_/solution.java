cf23ec247941091daef363da70ba52688a76f070
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-06-13 11:16:43 +0530

    Sync via AlgoSync

---
+++ [HR] Strings_ Reverse Each Word in a Sentence_spec_/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] w=s.split(" ");
+        // Your reversing logic is excellent and completely correct
+        for(int i = 0; i < w.length;i++) {
+        }
+        
+        
+    }    
+}
+        
+        
+        
+            String rev=new StringBuilder(w[i]).reverse().toString();
+            System.out.print(rev+" ");
