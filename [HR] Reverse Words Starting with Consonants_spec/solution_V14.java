39594f3b390ecea5b91a56028fc4c381c74b02fe
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-06-23 13:33:35 +0530

    Sync via AlgoSync

--- [HR] Reverse Words Starting with Consonants_spec/solution_V14.java
+++ [HR] Reverse Words Starting with Consonants_spec/solution_V14.java
@@ -1,29 +1,31 @@
-import java.io.*;
-import java.util.*;
-
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named 
-        Solution. */
-         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-
-        String result = "";
-        int n = s.length();
-        int i = 0;
-
-        while (i < n) {
-            // 1. Handle spaces exactly as they are
-            if (s.charAt(i) == ' ') {
-                result += " ";
-                i++;
-                continue;
-            }
-
-            // 2. Find boundaries of the current word
-            int start = i;
-            while (i < n && s.charAt(i) != ' ') {
-                i++;
             }
             int end = i - 1;
+
+            // 3. Brute Force Condition Check
+            if (isConsonant(s.charAt(start))) {
+                // Brute Force Inner Loop: Append characters backward from the word's end index
+                for (int j = end; j >= start; j--) {
+                    result += s.charAt(j);
+                }
+            } else {
+                // Straight Copy Loop: Append characters forward from the word's start index
+                for (int j = start; j <= end; j++) {
+                    result += s.charAt(j);
+                }
+            }
+        }
+
+                i++;
+            int start = i;
+            while (i < n && s.charAt(i) != ' ') {
+
+            // 2. Find boundaries of the current word
+            }
+                continue;
+                i++;
+                result += " ";
+            if (s.charAt(i) == ' ') {
+            // 1. Handle spaces exactly as they are
+        while (i < n) {
+
+        int i = 0;
