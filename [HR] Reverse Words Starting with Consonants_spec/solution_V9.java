7aaabfd1cc3e28d1f9b503a17ba4c7e36ad727ce
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-06-23 13:31:15 +0530

    Sync via AlgoSync

---
+++ [HR] Reverse Words Starting with Consonants_spec/solution_V9.java
@@ -0,0 +1,31 @@
+            }
+            int end = i - 1;
+
+            while (i < n && s.charAt(i) != ' ') {
+                i++;
+            int start = i;
+            // 2. Find boundaries of the current word
+
+            }
+                i++;
+                continue;
+            if (s.charAt(i) == ' ') {
+                result += " ";
+        int i = 0;
+
+        while (i < n) {
+            // 1. Handle spaces exactly as they are
+
+        String result = "";
+        int n = s.length();
+         Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+            // 3. Brute Force Condition Check
+            if (isConsonant(s.charAt(start))) {
+        Solution. */
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named 
+    public static void main(String[] args) {
+                // Brute Force Inner Loop: Append characters backward from the word's end index
+                for (int j = end; j >= start; j--) {
+                    result += s.charAt(j);
+                }
