8d1ccb1e230c11c3241cb1f6a2cd5c94aca9d282
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-07-02 10:24:28 +0530

    Sync via AlgoSync

---
+++ [HR] Extract Domain Name_Crtprep/solution.java
@@ -0,0 +1,30 @@
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        char[] arr = s.toCharArray();
+        String res = "";
+        boolean flag = false;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (arr[i] == '@') {
+                flag = true;
+                continue;
+            }
+
+            if (flag == true) {
+                if (arr[i] == '.')
+                    break;
+
+                res += arr[i];
+            }
+        }
+
+        System.out.println(res);
+    }
+}
