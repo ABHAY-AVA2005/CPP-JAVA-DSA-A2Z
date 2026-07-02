f812ca2fa8c3dc40ad0c90e481801ee9a09ca4b5
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-07-02 11:09:13 +0530

    Sync via AlgoSync

---
+++ [HR] Validate and Transform Email_Crtprep/solution.java
@@ -0,0 +1,30 @@
+
+        if(username.length()<3){
+            System.out.println("Invalid Email");
+            return;
+        }
+
+        String username=s.substring(0,idx);
+        }
+
+            System.out.println("Invalid Email");
+            return;
+
+        if(dot==-1){
+            return;
+        }
+
+        int dot=s.indexOf('.',idx);
+        if(idx==-1||idx!=s.lastIndexOf('@')){
+            System.out.println("Invalid Email");
+        int idx=s.indexOf('@');
+
+        String s=sc.next();
+
+        Scanner sc=new Scanner(System.in);
+
+    public static void main(String[]args){
+
+public class Solution{
+
+import java.util.*;
