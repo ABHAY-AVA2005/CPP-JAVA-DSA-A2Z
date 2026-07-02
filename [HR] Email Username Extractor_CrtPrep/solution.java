34173ef2bec6117582c7ce9bfaee7002ff30e6e6
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2026-07-02 10:13:11 +0530

    Sync via AlgoSync

---
+++ [HR] Email Username Extractor_CrtPrep/solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named 
+        Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        char[] arr=s.toCharArray();
+        String res="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(arr[i]=='@')
+            {
+                break;
+            }
+            
+            res+=arr[i];
+        }
+        
+        System.out.println(res);
+        
+    }
+}
